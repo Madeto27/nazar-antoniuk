183fa62afcb4f972de5b6e8a24e9c5f74a7d1ec6
nazikus <nitronazar@gmail.com>
2024-05-14 20:04:59 +0300

    Second commit

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,7 +20,7 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'madeto27', // Usually your GitHub org/user name.
+  organizationName: 'Madeto27', // Usually your GitHub org/user name.
   projectName: 'nazar-antoniuk', // Usually your repo name.
 
   onBrokenLinks: 'throw',
